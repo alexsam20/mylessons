67588e212fe180d9a72d98c495893d0feabedf05
Alex SaM <alsam3637@gmail.com>
2025-03-19 21:27:15 -0400

    add laminas/laminas-httphandlerrunner

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 
-use Framework\Http\ResponseSender;
 use Laminas\Diactoros\Response\HtmlResponse;
 use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
@@ -19,5 +19,5 @@ $name = $request->getQueryParams()['name'] ?? 'Guest';
 $response = (new HtmlResponse('Hello, ' . $name . '!'))
     ->withHeader('X-Developer', 'AlexSaM');
 
-$emitter = new ResponseSender();
-$emitter->send($response);
+$emitter = new SapiEmitter();
+$emitter->emit($response);

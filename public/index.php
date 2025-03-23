432bcc59116283615cddf23376b9c7aca5ef61f8
Alex SaM <alsam3637@gmail.com>
2025-03-23 14:28:35 -0400

    Create readonly ErrorHandlerMiddleware class

--- public/index.php
+++ public/index.php
@@ -8,11 +8,13 @@ use Framework\Http\Application;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Exception\RequestNotMatchedException;
+use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\JsonResponse;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
+ini_set('display_errors', 'Off');
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
@@ -31,6 +33,7 @@ function print_pre(mixed $var, int|bool|string $flag = 0): void
 ### Initialization
 
 $params = [
+    'debug' => false,
     'users' => ['admin' => 'password', 'alex' => '12345678']
 ];
 
@@ -53,8 +56,8 @@ $router = new AuraRouterAdapter($aura);
 $resolver = new MiddlewareResolver();
 $app = new Application($resolver, new Middleware\NotFoundHandler());
 
+$app->pipe(new Middleware\ErrorHandlerMiddleware($params['debug']));
 $app->pipe(Middleware\CredentialsMiddleware::class);
-
 $app->pipe(Middleware\ProfilerMiddleware::class);
 ### Running
 
---
+++ src/App/Http/Middleware/ErrorHandlerMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\JsonResponse;
+use Psr\Http\Message\ServerRequestInterface;
+use Throwable;
+
+readonly class ErrorHandlerMiddleware
+{
+    public function __construct(private bool $debug = false)
+    { }
+
+    public function __invoke(ServerRequestInterface $request, callable $next)
+    {
+        try {
+            return $next($request);
+        } catch (Throwable $e) {
+            if ($this->debug) {
+                return new JsonResponse([
+                    'error' => 'Server error',
+                    'code' => $e->getCode(),
+                    'message' => $e->getMessage(),
+                    'trace' => $e->getTrace(),
+                ], 500);
+            }
+            return new HtmlResponse('Server error', 500);
+        }
+    }
+}
